5fe116391686d7fdab643ba2d56a80e88b3c1e20
Jason Cobb <jason.e.cobb@gmail.com>
2017-08-13 23:56:26 +0000

    Release 2.3.8

--- src/main/java/com/srgood/reasons/impl/Reference.java
+++ src/main/java/com/srgood/reasons/impl/Reference.java
@@ -16,7 +16,7 @@ public class Reference {
     public static final String[] EIGHT_BALL = {"It is certain","It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Dount count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"};
     //see http://stackoverflow.com/questions/396429/how-do-you-know-what-version-number-to-use
 
-    public static final String VERSION = "2.3.7";
+    public static final String VERSION = "2.3.8";
     public static final List<String> LIBRARIES = Collections.unmodifiableList(Arrays.asList(
             "JDA (https://github.com/DV8FromTheWorld/JDA)",
             "JGit (https://eclipse.org/jgit/)",
