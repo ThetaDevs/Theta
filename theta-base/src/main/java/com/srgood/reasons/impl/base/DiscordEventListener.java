d0d45ac3ab34b28340dbb41e61e7a11024b7f3b6
DerekGoodnow <dmanlee10@gmail.com>
2018-04-15 08:12:00 -0700

    Restore body of DiscordEventListener#onGuildVoiceLeave

--- theta-base/src/main/java/com/srgood/reasons/impl/base/DiscordEventListener.java
+++ theta-base/src/main/java/com/srgood/reasons/impl/base/DiscordEventListener.java
@@ -94,11 +94,9 @@ public class DiscordEventListener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
+        // Leave is bot is the only user left
         if (event.getChannelLeft().getMembers().size() == 1) {
-            // TODO FIX AUDIO
-            //            AudioManager manager = event.getGuild();
-            //
-            //            manager.closeAudioConnection();
+            event.getGuild().getAudioManager().closeAudioConnection();
         }
     }
 
